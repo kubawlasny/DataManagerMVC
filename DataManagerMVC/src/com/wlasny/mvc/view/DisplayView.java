50c7930b029a04bb1d5635689c4b1550d8a15bf9
Jakub Własny <jakub.wlasny@gmail.com>
2016-06-01 06:49:38 +0200

    Initial commit

---
+++ DataManagerMVC/src/com/wlasny/mvc/view/DisplayView.java
@@ -0,0 +1,7 @@
+package com.wlasny.mvc.view;
+
+import javax.swing.JFrame;
+
+public class DisplayView extends JFrame{
+	
+}
---
+++ DataManagerMVC/src/com/wlasny/mvc/view/ListView.java
@@ -0,0 +1,5 @@
+package com.wlasny.mvc.view;
+
+public class ListView {
+
+}
